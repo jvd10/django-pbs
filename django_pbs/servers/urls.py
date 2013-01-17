89cd2cb164710a4e616343996f594a1001899b67
Brian May <brian@microcomaustralia.com.au>
2013-01-17 11:02:25 +1100

    Remove obsolete references.

--- django_pbs/servers/urls.py
+++ django_pbs/servers/urls.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with django-pbs  If not, see <http://www.gnu.org/licenses/>.
 
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 
 urlpatterns = patterns('django_pbs.servers.views',
 
